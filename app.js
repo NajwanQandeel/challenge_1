6bdda723bebeaf6b0222307386ed3ffaf20e772b
NajwanQandeel <najwan.qandeel@gmail.com>
2020-11-02 18:27:31 +0200

    second push

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 
+var play_board = ['','','','','','','','',''];
+
+var board_container = document.querySelector('.play_area');
+var render_board = () => {
+    board_container.innerHTML = "";
+    play_board.forEach((el, i) => {
+        board_container.innerHTML = `<div id="b_${i}"  onclick="addPlayerMove(${i})">${play_board[i]}</div>`;
+    }
+}
+
+render_board();
+
+
+
+
+
+// document.querySelector('#b1').addEventListener('click', event => {
+//     "#b1"="X";
+//   })
 
